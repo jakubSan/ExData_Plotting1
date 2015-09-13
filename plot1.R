dbc7061e0361aa3fca6ab87fb96a64700f207e9a
jakubSan <kuba.sanojca@gmail.com>
2015-09-14 00:49:28 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,15 +1,36 @@
 
-power <- read.table('household_power_consumption.txt')
+#Plot1.R
 
+#Create "data" directory for storing data if doesn't exist
+if(!file.exists("data")){
+  dir.create("data")
+}
+
+#Download the data
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "./data/household_power_consumption.zip", method = "internal")
+
+#Unzip the file
+unzip("./data/household_power_consumption.zip")
+
+#Read in downloaded file 
+power <- read.table('household_power_consumption.txt', header = TRUE, sep = ";", na.strings = "?")
+
+#Convert the date
 power$newDate <- as.Date(power$Date, format = '%d/%m/%Y')
 
+#Subset the datat only for 1-2.02.2007
 pow <- subset(power, power$newDate == '2007-02-01' | power$newDate == '2007-02-02')
 
-pow$GApow <- type.convert(as.character(pow$Global_active_power), dec = '.')
+#Convert the date and time
+pow$datetime <- strptime(paste(pow$Date,pow$Time, sep = " "), format = "%d/%m/%Y %H:%M:%S")
 
+#Open the png device
 png('plot1.png', width = 480, height = 480)
 
-hist(pow$GApow, col = 'red', main = 'Global Active Power',
+#Plot the histogram with appropriate labels
+hist(pow$Global_active_power, col = 'red', main = 'Global Active Power',
      xlab = 'Global Active Power (kilowatts)', ylab = 'Frequency')
 
+#Close the device
 dev.off()
